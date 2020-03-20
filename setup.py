e651257d0854767479dd6475cc0a821fd0a62d44
Kushal Das <mail@kushaldas.in>
2020-03-20 13:35:50 +0530

    Adds setup.py and MANIFEST for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""ManualBox project"""
+from setuptools import find_packages, setup
+
+setup(
+    name="manualbox",
+    version="0.1.0",
+    description="A secure storage as file system which requires user input to access files.",
+    long_description="A simple testing system which can be maintained.",
+    platforms=["Linux", "Darwin"],
+    author="Kushal Das",
+    author_email="mail@kushaldas.in",
+    url="https://manualbox.org",
+    license="GPLv3+",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "manualbox = manualbox:main",
+            "manualboxinput = manualbox.manualinput:main",
+        ]
+    },
+)
